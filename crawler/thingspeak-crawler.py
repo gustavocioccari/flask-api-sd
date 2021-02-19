fde0f451fa5814517b39057037ac1a80fdf887e1
gustavocioccari <gustavocioccari@gmail.com>
2021-02-19 17:13:32 -0300

    Implement db connection to crawler

--- crawler/thingspeak-crawler.py
+++ crawler/thingspeak-crawler.py
@@ -2,11 +2,13 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 from datetime import datetime
+import sqlite3
+from sqlite3 import Error
 import os
 
-api_key = 'V8N5Q53BS4XRUQW7'
-base_url = 'https://api.thingspeak.com/channels/'
+api_key = INSERT YOUR KEY HERE
 
+base_url = 'https://api.thingspeak.com/channels/'
 query = '1297494/feeds.json?api_key='+api_key
 
 session = Session()
@@ -21,18 +23,25 @@ channel_name = results['channel']['name']
 sensor_name = results['channel']['field1']
 sensor_description = results['channel']['description']
 sensor_unit = "BPM"
-sensor_reading = []
+sensor_readings = []
 reading_times = []
 
 for item in results['feeds']:
-  sensor_reading.append(item['field1'])
+  sensor_readings.append(item['field1'])
   reading_times.append(item['created_at'])
 
-print(channel_name)
-print(sensor_name)
-print(sensor_description)
-print(sensor_unit)
-print("Leituras do sensor: ")
-print(sensor_reading)
-print("Hora das leituras: ")
-print(reading_times)
+database = r'C:\Users\Gustavo\Documents\Graduação\Engenharia Mecatrônica\Optativas\Sistemas Distribuídos\FLASK-API\db\dbsqlite.db'
+
+try:
+  conn = sqlite3.connect(database)
+except(ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+for index, sensor_reading in enumerate(sensor_readings):
+  sql = ''' INSERT INTO sensor_readings (channel_name,sensor_name,sensor_description,sensor_unit,sensor_reading,reading_time)
+          VALUES(?,?,?,?,?,?) '''
+  sensor_reading = (channel_name,sensor_name,sensor_description,sensor_unit,sensor_reading,reading_times[index]);
+  cur = conn.cursor()
+  cur.execute(sql, sensor_reading)
+  conn.commit()
+conn.close()
