80eb7baad15385c6bc04a67a1caa35e862096b0d
gustavocioccari <gustavocioccari@gmail.com>
2021-03-02 17:06:06 -0300

    Refactor database path. Restore db data

--- crawler/thingspeak-crawler.py
+++ crawler/thingspeak-crawler.py
@@ -6,7 +6,7 @@ import sqlite3
 from sqlite3 import Error
 import os
 
-api_key = INSERT YOUR KEY HERE
+api_key = 'V8N5Q53BS4XRUQW7'
 
 base_url = 'https://api.thingspeak.com/channels/'
 query = '1297494/feeds.json?api_key='+api_key
@@ -30,7 +30,7 @@ for item in results['feeds']:
   sensor_readings.append(item['field1'])
   reading_times.append(item['created_at'])
 
-database = r'C:\Users\Gustavo\Documents\Graduação\Engenharia Mecatrônica\Optativas\Sistemas Distribuídos\FLASK-API\db\dbsqlite.db'
+database = 'db\dbsqlite.db'
 
 try:
   conn = sqlite3.connect(database)
